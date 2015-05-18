1328223e56c61b8e0638b45279932fce145b3c84
Igor <russion2005@gmail.com>
2015-05-18 22:38:37 +0300

    fix css linkages with html

--- src/main/java/com/release/fileUploader/FileUploader.java
+++ src/main/java/com/release/fileUploader/FileUploader.java
@@ -7,10 +7,11 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application extends SpringBootServletInitializer {
+public class FileUploader extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(applicationClass, args);
@@ -21,5 +22,20 @@ public class Application extends SpringBootServletInitializer {
         return application.sources(applicationClass);
     }
 
-    private static Class<Application> applicationClass = Application.class;
+    private static Class<FileUploader> applicationClass = FileUploader.class;
+
+/*    @Bean
+         public ViewResolver viewResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setTemplateMode("XHTML");
+        templateResolver.setPrefix("static/");
+        templateResolver.setSuffix(".html");
+
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(templateResolver);
+
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(engine);
+        return viewResolver;
+    }*/
 }
